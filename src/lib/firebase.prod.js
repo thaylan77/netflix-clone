a7cf5598121bf0259f10a1cb8a1b44c1c41269d2
Thaylã Cavalcante <thaylancavalcante@gmail.com>
2021-09-03 16:47:05 -0300

    Update firebase.prod.js

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -6,12 +6,12 @@ import 'firebase/auth';
 import { seedDatabase } from '../seed';
 
 const config = {
-  apiKey: "AIzaSyBpzxBNl2ejKLAfXQ8MwbcbzibbZfRjM0A",
-  authDomain: "netflix-b61c2.firebaseapp.com",
-  projectId: "netflix-b61c2",
-  storageBucket: "netflix-b61c2.appspot.com",
-  messagingSenderId: "309284420028",
-  appId: "1:309284420028:web:d4c20bfeffd69db22393b9"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const firebase = Firebase.initializeApp(config);
